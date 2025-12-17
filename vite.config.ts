67fae6212ffdd75124a670ba98ca7744a59a38c3
hsjeong <hsjeong@evidnet.co.kr>
2025-12-17 13:34:49 +0900

    chore: remove obsolete bundle name from vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -31,7 +31,6 @@ export default defineConfig({
   build: {
     lib: {
       entry: "./src/index.ts",
-      name: "UiPublishTest",
       formats: ["es", "cjs"],
       fileName: (format) => `index.${format}.js`,
     },
